ef92f0b32989f0e3afc0855f16f4163953f15f48
qbj700 <qbj700@gmail.com>
2020-10-14 11:41:01 +0900

    split과 integer.parseInt를 사용해봤다

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -16,74 +16,76 @@ public class Main {
 		String article2_body= "";
 		
 		while(true) {
-		System.out.printf("명령어 : ");
-		String command = scanner.nextLine();
-		if( command.equals("article add")) {
+			System.out.printf("명령어 : ");
+			String command = scanner.nextLine();
 			
-			
-			System.out.println("==게시물 등록==");
-			++id;
-			String title;
-			String body;
-			
-				if( id==1 ) {			
-					System.out.printf("제목 : ");
-					title = scanner.nextLine();
-					System.out.printf("내용 : ");
-					body = scanner.nextLine();
-					
-					article1_id = id;
-					article1_title = title;
-					article1_body = body;
-					
-					System.out.printf("==%d번 게시물 등록되었습니다==\n",id);
-					}
-					
-					
-				
-				else if( id==2 ) {			
-					System.out.printf("제목 : ");
-					title = scanner.nextLine();
-					System.out.printf("내용 : ");
-					body = scanner.nextLine();
+			if( command.equals("article add")) {
+				System.out.println("==게시물 등록==");
+				++id;
+				String title;
+				String body;				
+					if( id==1 ) {			
+						System.out.printf("제목 : ");
+						title = scanner.nextLine();
+						System.out.printf("내용 : ");
+						body = scanner.nextLine();
 						
-					article2_id = id;
-					article2_title = title;
-					article2_body = body;
-					
-					System.out.printf("==%d번 게시물 등록되었습니다==\n",id);
-					}
-			
-				else {	System.out.println("==저장 공간이 부족합니다==");	}		
-			}
-			else if( command.equals("article list")) {
-			
-				System.out.println("==게시물 리스트==");
-			
-				if (id==0) {
-				System.out.println("==등록된 게시물이 없습니다==");
-				}
-				else {
-					if( id >= 1 ) {
-					System.out.printf("번호 : %d 제목 : %s\n", article1_id, article1_title);
-					}
-					if( id >= 2 ) {
-						System.out.printf("번호 : %d 제목 : %s\n", article2_id, article2_title);
-					}
-			}
-			
-			}else if( command.equals("system exit")) {
-				System.out.println("==프로그램 종료==");
-				break;
+						article1_id = id;
+						article1_title = title;
+						article1_body = body;
+						
+						System.out.printf("==%d번 게시물 등록되었습니다==\n",id);
+						}
+					else if( id==2 ) {			
+						System.out.printf("제목 : ");
+						title = scanner.nextLine();
+						System.out.printf("내용 : ");
+						body = scanner.nextLine();
+							
+						article2_id = id;
+						article2_title = title;
+						article2_body = body;
+						
+						System.out.printf("==%d번 게시물 등록되었습니다==\n",id);
+						}
 				
-				}else if(command.equals("article detail 1")) {
-					System.out.printf("번호 %d \n제목 : %s \n내용 : %s\n", article1_id, article1_title, article1_body);
-				}else if(command.equals("article detail 2")) {
-					System.out.printf("번호 %d \n제목 : %s \n내용 : %s\n", article2_id, article2_title, article2_body);
+					else {	System.out.println("==저장 공간이 부족합니다==");	}		
+						}
+			
+				else if( command.equals("article list")) {
+					System.out.println("==게시물 리스트==");
+					if (id==0) {
+						System.out.println("==등록된 게시물이 없습니다==");
+						}
+					else {
+						if( id >= 1 ) {
+							System.out.printf("번호 : %d 제목 : %s\n", article1_id, article1_title);
+							}
+						if( id >= 2 ) {
+							System.out.printf("번호 : %d 제목 : %s\n", article2_id, article2_title);
+							}
+						}
+				
+				}else if( command.equals("system exit")) {
+					System.out.println("==프로그램 종료==");
+					break;
+					
+				}else if(command.startsWith("article detail ")) {
+					int inputedId = 0;
+					inputedId = Integer.parseInt(command.split(" ")[2]);
+					if( inputedId == 1) {
+						System.out.printf("번호 %d \n제목 : %s \n내용 : %s\n", article1_id, article1_title, article1_body);
+						}
+					if( inputedId == 2) {
+						System.out.printf("번호 %d \n제목 : %s \n내용 : %s\n", article2_id, article2_title, article2_body);
+						}
+					if( inputedId >= 3) {
+						System.out.printf("==게시물이 없습니다==\n", article1_id, article1_title, article1_body);
+						}
 				}
-			else{
-				System.out.println("==지정되지 않은 명령어==");
-			}
+				else{
+					System.out.println("==지정되지 않은 명령어==");
+				}
 		}
 		scanner.close();
 	}
