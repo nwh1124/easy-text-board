8bc8574fe6f8364211b23f7ff5c93fc39ddee016
nwh1124 <nwh0716@gmail.com>
2020-10-15 18:12:01 +0900

    집에서 복습

---
+++ src/com/sbs/example/easytextboard/Article.java
@@ -0,0 +1,9 @@
+package com.sbs.example.easytextboard;
+
+public class Article {
+	
+	int id;
+	String title;
+	String body;
+
+}
---
+++ src/com/sbs/example/easytextboard/main.java
@@ -0,0 +1,10 @@
+package com.sbs.example.easytextboard;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		new App().run();
+	}
+}
