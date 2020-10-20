dc9660efa0c82a8062268a9994310c9492d308ca
nwh1124 <nwh0716@gmail.com>
2020-10-20 11:15:53 +0900

    게시판 삭제 기능

--- src/board/App.java
+++ src/board/App.java
@@ -17,6 +17,7 @@ public class App {
 		}
 
 		int id = 0;
+		int articlesCount = 0;
 		String command;
 		Scanner sc = new Scanner(System.in);
 		
@@ -25,7 +26,7 @@ public class App {
 			command = sc.nextLine();
 		
 			if(command.equals("article add")) {
-				if(id >= articles.length) {
+				if(articlesCount >= articles.length) {
 					System.out.println("==저장 공간이 부족합니다==");
 				}else {
 					System.out.printf("제목 : ");
@@ -33,19 +34,21 @@ public class App {
 					System.out.printf("내용 : ");
 					String body = sc.nextLine();
 					
-					articles[id].title = title;
-					articles[id].body = body;
+					articles[articlesCount].id = id+1;
+					articles[articlesCount].title = title;
+					articles[articlesCount].body = body;
 					id++;
+					articlesCount++;
 					System.out.printf("%d번 글이이 생성되었습니다\n",id);
 				}
 				
 			}else if(command.equals("article list")) {
 				System.out.println("==게시물 리스트==");
-				if(id == 0) {
+				if(articlesCount == 0) {
 					System.out.println("==생성된 게시물이 없습니다==");
 				}
 				else {
-					for(int i = 0; i < id; i++) {
+					for(int i = 0; i < articlesCount; i++) {
 						System.out.printf("번호 : %d\n제목: %s\n",articles[i].id,articles[i].title);
 					}
 				}
@@ -53,15 +56,39 @@ public class App {
 			}else if(command.startsWith("article detail ")) {
 				String[] commandBits = command.split(" ");
 				int inputedId = Integer.parseInt(commandBits[2]);
-				if(inputedId > id || inputedId <= 0) {
+				if(inputedId > articlesCount || inputedId <= 0) {
 					System.out.printf("==%d번 글이 존재하지 않습니다==\n",inputedId);					
 				}
 				else {
 					System.out.printf("번호 %d\n제목 %s\n내용 %s\n",
 							articles[(inputedId-1)].id,articles[(inputedId-1)].title,articles[(inputedId-1)].body);
 				}
+							
+			}else if(command.startsWith("article delete ")) {
+				String[] commandBits = command.split(" ");
+				int inputedId = Integer.parseInt(commandBits[2]);
 				
-			}else if(command.equals("system exit")) {
+				if(inputedId <= 0 || inputedId > articlesCount) {
+					System.out.println("==%d 번 게시물은 존재하지 않습니다==");
+				}
+				else {
+					System.out.printf("==%d번 게시물이 삭제되었습니다==\n",inputedId);
+					
+					for(int i = 0; i < articles.length; i++) {
+						if(inputedId == articles[i].id) {
+							while(i <= articlesCount ) {																	
+							articles[i].id = articles[i+1].id;
+							articles[i].title = articles[i+1].title;
+							articles[i].body = articles[i+1].body;
+							i ++;
+							}
+						}
+					}									
+				articlesCount = articlesCount - 1 ;
+				}
+			}
+			
+			else if(command.equals("system exit")) {
 				System.out.println("==종료==");
 				break;
 				
